bece745521dd3dad30f06560709c3823eb45b3e7
exis <gotexis@gmail.com>
2019-11-15 22:52:21 +1100

    make function to rename to sequential

--- toolbox/rename.py
+++ toolbox/rename.py
@@ -4,12 +4,11 @@ import os, re
 Batch renaming files
 """
 
-path = os.path.dirname(__file__)
 
-
-def search_replace(find: str, replace: str) -> None:
+def search_replace(path: str, find: str, replace: str) -> None:
     """
     Case insensitive find and replace in a folder, recursively.
+    :param path:
     :param find:
     :param replace:
     :return:
@@ -23,3 +22,23 @@ def search_replace(find: str, replace: str) -> None:
                 print(root + os.sep + filename, ' => ', new_filename)
                 # do the rename
                 os.rename(root + os.sep + filename, root + os.sep + new_filename)
+
+
+def rename_to_sequential(path: str, ext: str = 'jpg', prefix: str = '') -> None:
+    """
+    Case insensitive find and replace in a folder, recursively.
+    :param ext:
+    :param path:
+    :return:
+    """
+    counter = 1
+
+    for root, dirs, files in os.walk(path):
+        for filename in files:
+            new_filename = f'{prefix}{counter}.{ext}'
+            if filename != new_filename:
+                # Print which file will be changed
+                print(root + os.sep + filename, ' => ', new_filename)
+                # do the rename
+                os.rename(root + os.sep + filename, root + os.sep + new_filename)
+                counter += 1
